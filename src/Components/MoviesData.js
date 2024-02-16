9ce13c09bc1cdbd1286640086d428e94a0353edd
djordjekristic <strelac.989@gmail.com>
2024-02-17 00:58:16 +0100

    function for img and info

--- src/Components/MoviesData.js
+++ src/Components/MoviesData.js
@@ -1,23 +1,31 @@
 import { useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieCard from "../Templates/snippets/movieCard";
 
 const MoviesData = () =>{
 
     let [moveName,setMoveName] = useState();
+    let[movieData,setMovieData] = useState({});
 
     const searchMovie = (e)=>{
         axios(process.env.REACT_APP_OMDBAPI_URL+'?t='+moveName+'&apikey='+process.env.REACT_APP_OMDBAPI_KEY)
-    .then(response =>console.log( response.data))
+    .then(response => setMovieData(response.data))
     .catch(error => alert('something is wrong'));
     }
     return(
         <>
+        <form onSubmit={e=>{e.preventDefault()}} className="mb-2">
         <input onInput={e=> setMoveName(e.currentTarget.value)}  type="text" placeholder="enter the movies name"/>
         <button onClick={searchMovie}  type="button">Find</button>
+        </form>
 
+        <MovieCard movie={movieData}/>
+        
+        
         </>
     )
 }
 
-export default MoviesData
+export default MoviesData;
 //
